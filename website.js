d413f1153e841da86b4a803e4c72379db0446619
Ayush Soni <mr.soni1002001@gmail.com>
2021-07-08 20:28:27 +0530

    initial commit

--- website.js
+++ website.js
@@ -164,3 +164,41 @@ document.getElementById("heading").addEventListener("click", function (e) {
   console.log(variable);
   // location.href = '//codewithharry.com'
 });
+console.clear();
+
+// MORE ON JS EVENTS
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", func1);
+btn.addEventListener("dblclick", func2);
+btn.addEventListener("mousedown", func3);
+
+function func1(e) {
+  console.log("Thanks", e);
+}
+
+function func2(e) {
+  console.log("Thanks for double click", e);
+}
+
+function func3(e) {
+  console.log("Thanks for mousedown click", e);
+}
+
+document.querySelector(".no").addEventListener("mouseenter", function () {
+  console.log("You entered no");
+});
+
+document.querySelector(".no").addEventListener("mouseleave", function () {
+  console.log("You exited no");
+});
+
+document
+  .querySelector(".container")
+  .addEventListener("mousemove", function (e) {
+    document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${
+      e.offsetX + e.offsetY
+    })`;
+  });
+
+console.clear();
