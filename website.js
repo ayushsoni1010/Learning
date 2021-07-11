24f744c3b128ae0e0c4270d4c3bb65de4117cc1f
Ayush Soni <mr.soni1002001@gmail.com>
2021-07-11 18:01:16 +0530

    Initial commit

--- website.js
+++ website.js
@@ -147,12 +147,6 @@ console.log(elem2, pr);
 // quick quiz
 // create a heading element with text as "Go to CodeWithHarry" and create an a tag outside it with href = "https://www.codewithharry.com"
 
-
-
-
-
-
-
 // EVENTS & EVENTS OBJECT
 
 document.getElementById("heading").addEventListener("click", function (e) {
@@ -172,58 +166,46 @@ document.getElementById("heading").addEventListener("click", function (e) {
 });
 console.clear();
 
-
-
-
-
 // MORE ON JS EVENTS
 
-let btn=document.getElementById('btn');
-btn.addEventListener('click',func1);
-btn.addEventListener('dblclick',func2);
-btn.addEventListener('mousedown',func3);
+let btn = document.getElementById("btn");
+btn.addEventListener("click", func1);
+btn.addEventListener("dblclick", func2);
+btn.addEventListener("mousedown", func3);
 
-function func1(e){
-  console.log("Thanks",e);
+function func1(e) {
+  console.log("Thanks", e);
 }
 
-function func2(e){
-  console.log("Thanks for double click",e);
+function func2(e) {
+  console.log("Thanks for double click", e);
 }
 
-function func3(e){
-  console.log("Thanks for mousedown click",e);
+function func3(e) {
+  console.log("Thanks for mousedown click", e);
 }
 
-
-
-document.querySelector('.no').addEventListener('mouseenter',function(){
-  console.log('You entered no');
+document.querySelector(".no").addEventListener("mouseenter", function () {
+  console.log("You entered no");
 });
 
-document.querySelector('.no').addEventListener('mouseleave',function(){
-  console.log('You exited no');
+document.querySelector(".no").addEventListener("mouseleave", function () {
+  console.log("You exited no");
 });
 
-document.querySelector('.container').addEventListener('mousemove',function(e){
-  document.body.style.backgroundColor=`rgb(${e.offsetX},${e.offsetY},${e.offsetX + e.offsetY})`;
-});
+document
+  .querySelector(".container")
+  .addEventListener("mousemove", function (e) {
+    document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${
+      e.offsetX + e.offsetY
+    })`;
+  });
 
 console.clear();
 
-
-
-
-
-
-
-
-
-
-
 // LOCAL & SESSION STORAGE IN JS
 
-let impArray = ['adrak', 'pyaz', 'bhindi'];
+let impArray = ["adrak", "pyaz", "bhindi"];
 
 // // Add a key-value pair inside local Storage
 // localStorage.setItem('Name', 'Harry');
@@ -254,6 +236,43 @@ let impArray = ['adrak', 'pyaz', 'bhindi'];
 
 
 
+// MATH OBJECT IN JS
 
+let x = 3;
+let y = 6;
+let z;
+z = x + y;
+z = x - y;
+z = x * y;
+z = x / y;
 
+// Exploring the Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.4);
+z = Math.ceil(5.3);
+z = Math.floor(-5.3);
+z = Math.abs(5);
+z = Math.sqrt(64);
+z = Math.pow(2, 3);
+z = Math.min(2, 3, 34, 234, 2342, 34);
+z = Math.max(2, 3, 34, 234, 34);
+z = Math.random();
+z = 100 * Math.random();
+z = Math.ceil(50 + (100 - 50) * Math.random());
+// a = (0, 1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
 
+console.log(z);
+
+// 3
+// 2.8
+// 2
+// 1
+// 0
+// -1
+// -2
+// -2.7
+// -3
